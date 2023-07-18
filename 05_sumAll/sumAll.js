ab82774d53dd17d1c6172238497c82638295cded
joris <jorisde.denelzen@gmail.com>
2023-07-18 18:31:04 +0200

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof(x) != "number" || typeof(y) != "number") {
+        return "ERROR"
+    }
+    if (y < x) {
+        const temp = x;
+        x = y;
+        y = temp;
+    }
+
+    if (x < 0) {
+        return "ERROR";
+    }
+
+    var sum = 0;
+    for (i = x; i <= y; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
