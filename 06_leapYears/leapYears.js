9f6cbf0ebff5285b3bc542d77b62acf7d68897f0
joris <jorisde.denelzen@gmail.com>
2023-07-18 18:38:13 +0200

    06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(y) {
+    if (y % 4 == 0 ) {
+        if (y % 100 != 0) {
+            return true;
+        }
+        if (y % 400 == 0) {
+            return true;
+        }
+    }
+    return false;
+
+    
 
 };
 
